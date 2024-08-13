c0b4518a40ff92cf839dd06da6e454ad4315bf1b
AbdullatifHabiba <es-abdellatifkhalid2024@alexu.edu.eg>
2024-08-13 08:49:36 +0300

    refactor: Update API endpoints and fetch data in hotels page

--- app/ui/hotels/page.js
+++ app/ui/hotels/page.js
@@ -1,17 +1,13 @@
 import Navbar from "@/Components/Navbar";
 import ShopHotels from "@/Components/explore/hotels";
-import {hotels} from "@/lib/data";
 
 
 export default async function Hotels() {
     const API_URL = process.env.SERVER_URL;
-    let hotels = hotels;
-    try {
-        hotels = await fetch(`${API_URL}/api/hotels`).then((res) => res.json());
-    }
-    catch (error) {
-        console.error("An error occurred:", error);
-    }
+ 
+    const  hotels = await fetch(`${API_URL}/api/hotels`).then((res) => res.json());
+    
+    
     return (
         <>
         <Navbar />
