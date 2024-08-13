3d7bd448f41d7895f7b57088b4a63e41a28d3842
AbdullatifHabiba <es-abdellatifkhalid2024@alexu.edu.eg>
2024-08-13 11:47:54 +0300

    refactor: Update API endpoints and fetch data in flights/search/page.js
    This commit updates the API endpoints and fetches data in the flights/search/page.js file. The changes include updating the API URL to use the environment variable SERVER_URL and fetching flight details, unique places, and deals from the API.

--- app/ui/flights/search/page.js
+++ app/ui/flights/search/page.js
@@ -1,17 +1,16 @@
-'use client';
 import FlightDetails from "@/Components/flight-details/FlightDetails";
 import Navbar from "@/Components/Navbar";
-import { useRouter } from "next/navigation";
 
 export default async function search() {
-  const API_URL = 'http://localhost:3000';
- 
-   const deals = await fetch(`${API_URL}/api/deals`).then((res) => res.json());
-   const  uniquePlaces = await fetch(`${API_URL}/api/places`).then((res) => res.json());
-   const flightDetailsData = await fetch(`${API_URL}/api/flights`).then((res) => res.json());
-   const router = useRouter();
-   const { from, to, date, passengers } = router.query;
-    console.log(from, to, date, passengers);
+  const API_URL = process.env.SERVER_URL;
+  const flightDetailsData = await fetch(`${API_URL}/api/flights`).then((res) =>
+    res.json()
+  );
+  const uniquePlaces = await fetch(`${API_URL}/api/places`).then((res) =>
+    res.json()
+  );
+  const deals = await fetch(`${API_URL}/api/deals`).then((res) => res.json());
+
   return (
     <>
       <Navbar />
