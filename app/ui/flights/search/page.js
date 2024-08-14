92db6ad1926d841d1780a8c803df2a1092471b9d
AbdullatifHabiba <es-abdellatifkhalid2024@alexu.edu.eg>
2024-08-14 15:08:15 +0300

    feat: Update flight search API endpoint and implement lazy loading for images

--- app/ui/flights/search/page.js
+++ app/ui/flights/search/page.js
@@ -2,22 +2,23 @@ import FlightDetails from "@/Components/flight-details/FlightDetails";
 import Navbar from "@/Components/Navbar";
 
 export default async function search({ searchParams }) {
-
-
   const API_URL = process.env.SERVER_URL;
   const hasSearchParams = searchParams && Object.keys(searchParams).length > 0;
 
   const [flightDetailsData, uniquePlaces, deals] = await Promise.all([
     hasSearchParams
       ? fetch(
-          `${API_URL}/api/flights/search?from=${searchParams.from}&to=${searchParams.to}&startDate=${searchParams.startDate}&endDate=${searchParams.endDate}&adults=${searchParams.adults}&minors=${searchParams.minors}`
-          ,{ next: { revalidate: 10 } } // Revalidate every 3 seconds
+          `${API_URL}/api/flights/search?
+          from=${searchParams.from}&to=${searchParams.to}
+          &startDate=${searchParams.startDate}&endDate=${searchParams.endDate}
+          &adults=${searchParams.adults}&minors=${searchParams.minors}`,
+          { next: { revalidate: 10 } } // Revalidate every 10 seconds
         ).then((res) => res.json())
-      : fetch(`${API_URL}/api/flights`).then((res) => res.json()),
+      : fetch(`${API_URL}/api/flights`,  {next: { revalidate: 10 }}).then((res) => res.json()),
     fetch(`${API_URL}/api/places`).then((res) => res.json()),
     fetch(`${API_URL}/api/deals`).then((res) => res.json()),
   ]);
-console.log("flightDetailsData", flightDetailsData);
+  console.log("flightDetailsData", flightDetailsData);
   console.log("search", searchParams);
 
   return (
@@ -25,7 +26,6 @@ console.log("flightDetailsData", flightDetailsData);
       <Navbar />
       <div>
         <FlightDetails
-          
           flightDetailsData={flightDetailsData}
           uniquePlaces={uniquePlaces}
           deals={deals}
